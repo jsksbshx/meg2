2f1511283790f75b83bfc092122aa22a98b5edbb
jsksbshx <bgmi06088@gmail.com>
2025-02-24 19:21:26 +0530

    Update syed.py

--- syed.py
+++ syed.py
@@ -11,7 +11,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7272842335:AAFLwtA-8R_Vww7WJnRBsqmmWSwQjINVfL4')
+bot = telebot.TeleBot('7629044400:AAFrgyuhGOpfQJtW7vykB4OxJeilgt2hpc0')
 
 # Admin user IDs
 admin_id = ["1713986516"]
